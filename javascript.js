398c539a1b1d3dd816d231c84c88201eff83ba60
Spyrantis Theodoros <oshikurou@gmail.com>
2019-03-31 20:55:28 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+function showTime(){
+
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){5
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s + " " + session;
+    console.log(time);
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
